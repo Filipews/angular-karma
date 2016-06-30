5d74dfaaefb1c7c0ba5a1acf5a7186ca6385f29f
filipe <filipe.wang.silva@gmail.com>
2016-06-30 10:48:22 -0300

    Adjusted Mod1 and Mod2 test specs

--- test/unit/mod1spec.js
+++ test/unit/mod1spec.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
 
-	describe('app.mod1:module...', function() {
+	describe('app.mod1 - Module ....', function() {
 
 		beforeEach(module('app.core'));
 		beforeEach(module('app.mod1'));
 
-		describe('Mod1:controller...', function(){
+		describe('Mod1 - Controller ....', function(){
 
-			it('should be defined...', inject(function($controller) {
+			it('should be defined ....', inject(function($controller) {
 				//spec body
 				var mod1Ctrl = $controller('Mod1');
 				expect(mod1Ctrl).toBeDefined();
--- test/unit/mod2spec.js
+++ test/unit/mod2spec.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
 
-	describe('app.mod2:module...', function() {
+	describe('app.mod2 - Module ....', function() {
 
 		beforeEach(module('app.core'));
 		beforeEach(module('app.mod2'));
 
-		describe('Mod2:controller...', function(){
+		describe('Mod2 - Controller ....', function(){
 
-			it('should be defined...', inject(function($controller) {
+			it('should be defined ....', inject(function($controller) {
 				//spec body
 				var mod2Ctrl = $controller('Mod2');
 				expect(mod2Ctrl).toBeDefined();
