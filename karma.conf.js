1026be9dd937380a303df1562a33f5d899957ab9
filipe <filipe.wang.silva@gmail.com>
2016-06-30 10:20:40 -0300

    Adjusted karma configuration files to receive dev scripts

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,39 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      /* Vendor */
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-route/angular-route.min.js',
+      'node_modules/angular-animate/angular-animate.min.js',
+      'node_modules/angular-sanitize/angular-sanitize.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/toastr/build/toastr.min.js',
+      'node_modules/moment/min/moment.min.js',
+      /* Blocks */
+      'app/blocks/exception/exception.module.js',
+      'app/blocks/exception/exception-handler.provider.js',
+      'app/blocks/exception/exception.js',
+      'app/blocks/logger/logger.module.js',
+      'app/blocks/logger/logger.js',
+      'app/blocks/router/router.module.js',
+      'app/blocks/router/routehelper.js',
+      /* Core */
+      'app/core/core.module.js',
+      'app/core/constants.js',
+      'app/core/config.js',
+      'app/core/dataservice.js',
+      /* App */
+      'app/app.module.js',
+      /* Mod1 */
+      'app/mod1/mod1.module.js',
+      'app/mod1/config.route.js',
+      'app/mod1/mod1.js',
+      /* Mod2 */
+      'app/mod2/mod2.module.js',
+      'app/mod2/config.route.js',
+      'app/mod2/mod2.js',
+      /* Test specs */
       'test/unit/**/*spec.js'
     ],
 
